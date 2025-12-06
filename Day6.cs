0ad2eb8ced228e86c42268828596048dd076e2d7
vildmusen <viktorvilmusenaho@hotmail.com>
2025-12-06 11:19:28 +0100

    part 2

--- Day6.cs
+++ Day6.cs
@@ -1,6 +1,14 @@
+using System.Runtime.InteropServices;
+
 public static class Day6
 {
     public static void Run(string[] input)
+    {
+        Part1(input);
+        Part2(input);
+    }
+
+    public static void Part1(string[] input)
     {
         var rows = new List<List<long>>();
 
@@ -39,5 +47,55 @@ public static class Day6
                     rows[i].Aggregate((a, b) => a + b));
         
         Console.WriteLine(sums.Sum());
+    }
+
+    public static void Part2(string[] input)
+    {
+        long sum = 0;
+        var currentNums = new List<long>();
+
+        for (int i = input[0].Length - 1; i >= 0; i--)
+        {
+            var currentNum = "";
+            char? op = null;
+
+            for (int j = 0; j < input.Length; j++)
+            {
+                var current = input[j][i];
+
+                if (current == ' ')
+                {
+                    continue;
+                }
+
+                if (current == '+' || current == '*')
+                {
+                    op = current;
+                }
+                else
+                {
+                    currentNum += current;
+                }
+            }
+
+            if (currentNum == "")
+            {
+                continue;
+            }
+
+            currentNums.Add(long.Parse(currentNum.Trim()));
+
+            if (op != null)
+            {
+                sum += op == '+' ?
+                    currentNums.Aggregate((a, b) => a + b) :
+                    currentNums.Aggregate((a, b) => a * b);
+
+                currentNums.Clear();
+                op = null;
+            }
+        }
+
+        Console.WriteLine(sum);
     }
 }
